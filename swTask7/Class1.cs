e19015ba84dd684374b5f5c4fa104b8b9f166731
L1v1ngston <115081879+L1v1ngston@users.noreply.github.com>
2023-12-12 13:49:36 +0300

    add project

---
+++ swTask7/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swTask7
+{
+    internal class cntSolutions
+    {
+        int CntSolutions(double a, double b, double c)
+        {
+            double d = b * b - 4 * a * c;
+            if (d < 0) return 0;
+            else if (d == 0) return 1;
+            else return 2;
+        }
+    }
+}
