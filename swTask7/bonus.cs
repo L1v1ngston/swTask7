bd66cc8129923d01e1571a1e9bd3a50c8d338be3
L1v1ngston <115081879+L1v1ngston@users.noreply.github.com>
2023-12-12 16:54:49 +0300

    changes

--- swTask7/bonus.cs
+++ swTask7/bonus.cs
@@ -2,35 +2,42 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 class bonus
 {
-    static async Task Main(string[] args)
+    static async Task Main()
     {
-        var apiUrl = "https://tracker.yandex.ru/createTicket?queue=TEAMCITYBUILDFA&_form=false";
-        var apiToken = "y0_AgAAAABdx8V8AAr6sgAAAAD0lRNoGTu4N5UlS82mwTRCNB7t4tHHwLE";
-        var orgId = "bpfbmihbfa98v3kflolj";
-        var queueId = "TEAMCITYBUILDFA";
-        var ownerId = "aje5onvp7suh6hbr1jhh";
+        var apiToken = "y0_AgAAAABdx8V8AAr6sgAAAAD0lRNoGTu4N5UlS82mwTRCNB7t4tHHwLE"; // Замените на ваш токен
+        var queueId = "TEAMCITYBUILDFA";   // ID созданной очереди
+        var ownerId = "aje5onvp7suh6hbr1jhh";   // ID владельца очереди
+        var orgId = "bpfbmihbfa98v3kflolj";       // ID вашей организации в Yandex Tracker
 
-        using (var client = new HttpClient())
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", $"OAuth {apiToken}");
+        client.DefaultRequestHeaders.Add("X-Org-ID", orgId);
+
+        var url = "https://tracker.yandex.ru/createTicket?queue=TEAMCITYBUILDFA&_form=false";
+        var data = new
         {
-            client.DefaultRequestHeaders.Add("Authorization", $"OAuth {apiToken}");
-            client.DefaultRequestHeaders.Add("X-Org-ID", orgId);
+            queue = queueId,
+            summary = "Build Failure in TeamCity",
+            description = "A build has failed in TeamCity. Please investigate.",
+            assignee = ownerId
+        };
 
-            var json = $"{{\"queue\": \"{queueId}\", \"summary\": \"Build Failure in TeamCity\", \"description\": \"A build has failed in TeamCity. Please investigate.\", \"assignee\": \"{ownerId}\"}}";
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(url, content);
+        
 
-            var response = await client.PostAsync(apiUrl, content);
-
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Task created successfully");
-            }
-            else
-            {
-                Console.WriteLine($"Failed to create task: {response.StatusCode}");
-            }
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Task created successfully");
+        }
+        else
+        {
+            Console.WriteLine("Failed to create task");
         }
     }
 }
